911a1450001f132fc55789715f1064b42ca54934
liuhanru <isuekey@163.com>
2021-04-23 11:36:40 +0800

    dist

---
+++ dist/simple-uuidv4.esm.js
@@ -0,0 +1,34 @@
+var hexArray = Object.freeze(new Array(256).fill(0).map(function (ele, idx) {
+  return idx.toString(16).padStart(2, '0').toLowerCase();
+}));
+
+var randomIndex = function () {
+  return Math.floor(Math.random() * 256);
+};
+
+var randomHex = function (size) {
+  if ( size === void 0 ) size=0;
+
+  return new Array(size).fill(0).map(function (ele) {
+    return hexArray[randomIndex()];
+  });
+};
+
+var uuid = function (divider) {
+  if ( divider === void 0 ) divider="-";
+
+  return randomHex(4).concat( [divider],
+    randomHex(2), [divider],
+    randomHex(2), [divider],
+    randomHex(2), [divider],
+    randomHex(6) ).join('');
+};
+
+var simpleUUId = {
+  hexArray: hexArray,
+  randomHex: randomHex,
+  uuid: uuid
+};
+
+export default simpleUUId;
+export { hexArray, randomHex, randomIndex, uuid };
